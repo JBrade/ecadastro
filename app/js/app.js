f41dc5a319c18efe4d3e2927dfa59c6ea6e25b28
Darlan Damasio <damasio34@gmail.com>
2016-11-02 17:09:06 -0200

    Ajueste no serviço de autenticação

--- app/js/app.js
+++ app/js/app.js
@@ -1,51 +1,46 @@
-'use strict'
+(function(angular) {
 
-angular
-    .module('ecadastro', ['firebase', 'ngRoute', 'ngMaterial', 'ui.utils.masks'])
-    .config(function($routeProvider) {
+    'use strict'
 
-		$routeProvider.otherwise('/');
+    angular
+        .module('ecadastro', ['firebase', 'ngRoute', 'ngMaterial', 'ui.utils.masks'])
+        .config(function($routeProvider) {
 
-        $routeProvider
+    		$routeProvider.otherwise('/');
 
-            .when('/', {
-                templateUrl: 'views/inscricao-incluir-view.html',
-                controller: 'InscricaoIncluirController'
-            })
-            .when('/login', {
-                templateUrl : 'views/login-view.html',
-                controller: 'LoginController'
-            })
-            .when('/login/incluir', {
-                templateUrl : 'views/login-incluir-view.html',
-                controller: 'LoginIncluirController'
-            })
-            .when('/inscricao/listar', {
-                templateUrl : 'views/inscricao-listar-view.html',
-                controller: 'InscricaoListarController'
+            $routeProvider
+
+                .when('/', {
+                    templateUrl: 'views/inscricao-incluir-view.html',
+                    controller: 'InscricaoIncluirController'
+                })
+                .when('/login', {
+                    templateUrl : 'views/login-view.html',
+                    controller: 'LoginController'
+                })
+                .when('/login/incluir', {
+                    templateUrl : 'views/login-incluir-view.html',
+                    controller: 'LoginIncluirController'
+                })
+                .when('/inscricao/listar', {
+                    templateUrl : 'views/inscricao-listar-view.html',
+                    controller: 'InscricaoListarController'
+                });
+    	})
+        .config(function($firebaseRefProvider) {
+            $firebaseRefProvider.registerUrl('https://ecadastro-b9df0.firebaseio.com');
+        })
+        .config(function($provide) {
+            $provide.decorator('$firebaseArray', function($delegate, $window) {
+                var add = $delegate.prototype.$add;
+
+                $delegate.prototype.$add = function(newData) {
+                    newData.datahora = firebase.database.ServerValue.TIMESTAMP;
+                    return add.call(this, newData);
+                };
+
+                return $delegate;
             });
-	})
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('teal');
-    })
-    .config(function($firebaseRefProvider) {
-        $firebaseRefProvider.registerUrl('https://ecadastro-b9df0.firebaseio.com');
-    })
-    .config(function($provide) {
-        $provide.decorator('$firebaseArray', function($delegate, $window) {
-            var add = $delegate.prototype.$add;
-
-            $delegate.prototype.$add = function(newData) {
-                newData.datahora = firebase.database.ServerValue.TIMESTAMP;
-                return add.call(this, newData);
-            };
-
-            return $delegate;
         });
-    })
-    .factory('LoginService', LoginService);
 
-
-    function LoginService($firebaseRef, $firebaseAuth) {
-        return $firebaseAuth($firebaseRef.default);
-    };
+})(angular);
