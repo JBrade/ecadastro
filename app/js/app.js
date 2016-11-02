c0a103091825b86c59d82b7ec2e3c7c836a60331
Darlan Damasio <damasio34@gmail.com>
2016-11-02 14:22:34 -0200

    Inclusão da controller de login e de inscrição separados.

--- app/js/app.js
+++ app/js/app.js
@@ -25,6 +25,9 @@ angular
                 controller: 'InscricaoListarController'
             });
 	})
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('teal');
+    })
     .config(function($firebaseRefProvider) {
         $firebaseRefProvider.registerUrl('https://ecadastro-b9df0.firebaseio.com');
     })
@@ -41,53 +44,6 @@ angular
         });
     })
     .factory('LoginService', LoginService)
-    .controller('InscricaoIncluirController', function($scope, $filter, $mdDialog, $firebaseArray, $firebaseObject, $firebaseRef) {
-        var inscricoes = $firebaseArray($firebaseRef.default.child('inscricoes'));
-
-        $scope.Limpar = limpar
-        $scope.Cadastrar = cadastrar;
-        $scope.DesabilitaSalvar = desabilitaSalvar;
-
-        // -------------------------------------------------------------------
-
-        function alerta(titulo, mensagem) {
-            var alert = $mdDialog.alert({
-                title: titulo,
-                textContent: mensagem,
-                ok: 'Ok'
-            });
-
-            $mdDialog.show(alert);
-        }
-
-        function desabilitaSalvar(inscricaoForm) {
-            return !inscricaoForm.$valid;
-        };
-        function limpar(inscricaoForm) {
-            $scope.inscricao = null;
-            $scope.inscricao = angular.copy({});
-            $scope.inscricaoForm.$setPristine();
-        };
-        function cadastrar(inscricao) {
-            $firebaseObject($firebaseRef.default.child('inscricoes'))
-                .$ref()
-                .orderByChild('CPF').equalTo(inscricao.CPF)
-                .once('value', function(snap) {
-                    if (snap.val() === null) {
-                        inscricao.DataDeNascimento = $filter('date')(inscricao.DataDeNascimento, "dd/MM/yyyy");
-                        inscricoes.$add(inscricao).then(function(result) {
-                            alerta('Confirmação de cadastro', 'Cadastro foi efetuado com sucesso!');
-                            $scope.inscricao = null;
-                            // console.log('Usuário cadastrado com sucesso.');
-                        });
-                    } else {
-                        alerta('Cadastro não efetuado', 'CPF já cadastrado.');
-                        $scope.inscricao = null;
-                    };
-                });
-        };
-
-    })
     .controller('InscricaoListarController', function($scope, $firebaseArray, $firebaseRef) {
         var inscricoes = $firebaseArray($firebaseRef.default.child('inscricoes').orderByChild('Nome'));
 
@@ -95,6 +51,7 @@ angular
 
         // -------------------------------------------------------------------
     });
+
 
     function LoginService($firebaseRef, $firebaseAuth) {
         return $firebaseAuth($firebaseRef.default);
